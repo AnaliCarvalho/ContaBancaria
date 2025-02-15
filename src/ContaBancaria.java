e9598af3a3182d736046d7223ce8eb38c5e7bc1a
AnaliCarvalho <analilaracarvalho@gmail.com>
2025-02-15 17:15:38 -0300

    Salvando alterações

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class ContaBancaria {
+
+    private int numeroDaConta;
+    private double saldo;
+
+    public ContaBancaria(int numeroDaConta, double saldo) {
+        this.numeroDaConta = numeroDaConta;
+        this.saldo = saldo;
+    }
+
+
+    public int getNumeroDaConta() {
+        return numeroDaConta;
+    }
+
+    public void setNumeroDaConta(int numeroDaConta) {
+        this.numeroDaConta = numeroDaConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.util.ArrayList;
+import java.util.SortedMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        ContaBancaria conta1 = new ContaBancaria(444544, 7800.12);
+        ContaBancaria conta2 = new ContaBancaria(458871, 4500.12);
+        ContaBancaria conta3 = new ContaBancaria(100041, 7500.15);
+        ContaBancaria conta4 = new ContaBancaria(225874, 120.25);
+        ContaBancaria conta5 = new ContaBancaria(100548, 28054.10);
+
+        ArrayList<ContaBancaria> listaContas = new ArrayList<>();
+        listaContas.add(conta1);
+        listaContas.add(conta2);
+        listaContas.add(conta3);
+        listaContas.add(conta4);
+        listaContas.add(conta5);
+
+        ContaBancaria contaMaiorSaldo = listaContas.get(0);
+        for (ContaBancaria conta : listaContas) {
+            if (conta.getSaldo() > contaMaiorSaldo.getSaldo()) {
+                contaMaiorSaldo = conta;
+            }
+        }
+
+        ContaBancaria contaMenorSaldo = listaContas.get(0);
+        for (ContaBancaria conta : listaContas) {
+            if (conta.getSaldo() < contaMenorSaldo.getSaldo()) {
+                contaMenorSaldo = conta;
+            }
+        }
+
+        System.out.println("**** CONTA COM O MAIOR SALDO ****\n");
+        System.out.println("Número: " + contaMaiorSaldo.getNumeroDaConta() + "\nSaldo: R$ " + contaMaiorSaldo.getSaldo());
+        System.out.println("\n**** CONTA COM O MENOR SALDO ****\n");
+        System.out.println("Número da menor conta: " + contaMenorSaldo.getNumeroDaConta() + "\nSaldo: R$ " + contaMenorSaldo.getSaldo());
+
+
+
+
     }
 }
